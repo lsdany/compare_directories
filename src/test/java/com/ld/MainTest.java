6e8efe7975e80fc990bda8f58479254798328dbb
Lsdany <luisdany@gmail.com>
2019-06-14 15:34:38 -0600

    Adding tests, gradle modification

---
+++ src/test/java/com/ld/MainTest.java
@@ -0,0 +1,23 @@
+package com.ld;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    private Main main;
+
+    @After
+    public void init(){
+        main = new Main();
+    }
+
+    @Test
+    public void testInit(){
+        main = new Main();
+    }
+
+}
